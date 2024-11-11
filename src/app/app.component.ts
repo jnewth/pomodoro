e0f3e9f8e3e7384c62d3c4f30b7ef308a3fa5c6c
Joshua Newth <mrfurious@gmail.com>
2024-11-10 19:23:06 -0500

    basic adding of input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pomodoro';
   duration = 25;
-  tasks = ['task 1', 'task 2'];
+  tasks = new Array(); // ['task 1', 'task 2'];
+  currentItem = '';
 
   onClick() {
     console.log("click");
+  }
+
+  addItem() {
+    if (this.currentItem !== '') {
+      this.tasks.push(this.currentItem);
+      this.currentItem = '';
+    }
   }
 }
