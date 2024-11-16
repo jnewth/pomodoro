47bd8797ab3f7d6813cb1c8178c460a48c32d945
Joshua Newth <mrfurious@gmail.com>
2024-11-16 11:41:55 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
-const TASK_KEY = 'tasks';
+const TASK_LIST = 'tasks';
+const TASK_ID = 'tasks_id';
+const START_ID = 1;
+
+type Task = {
+  desc: string;
+  id: number;
+}
+
+function CreateTask(desc: string, id: number): Task {
+  return {
+    desc: desc,
+    id: id
+  };
+}
 
 @Component({
   selector: 'app-root',
@@ -12,11 +26,20 @@ const TASK_KEY = 'tasks';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-  title = 'pomodoro';
+  title = 'Pomodo';
   duration = 25;
-  tasks = new Array();
+  tasks = new Array<Task>(); //task is an id and a string value
+  nextId = START_ID;
   currentItem = '';
+
+  ngAfterViewInit() {
+    const tasks_string = window.localStorage.getItem(TASK_LIST);
+    const tasks_id = window.localStorage.getItem(TASK_ID)
+    if (tasks_string != null && tasks_id != null) {
+      this.tasks = JSON.parse(tasks_string);
+      this.nextId = JSON.parse(tasks_id);
+    }
+  }
 
   onClick() {
     console.log("click");
@@ -24,17 +47,19 @@ export class AppComponent {
 
   addItem() {
     if (this.currentItem !== '') {
-      this.tasks.unshift(this.currentItem);
+      this.tasks.unshift(CreateTask(this.currentItem, this.nextId++));
       this.currentItem = '';
-      window.localStorage.setItem(TASK_KEY, JSON.stringify(this.tasks));
+      window.localStorage.setItem(TASK_LIST, JSON.stringify(this.tasks));
+      window.localStorage.setItem(TASK_ID, JSON.stringify(this.nextId));
     }
   }
-
-  ngAfterViewInit(){
-    const tasks_string = window.localStorage.getItem('tasks');
-    if (tasks_string != null) {
-      this.tasks = JSON.parse(tasks_string);
-    }
+  deleteItem(task:Task) {
+    let index = this.tasks.indexOf(task);
+    this.tasks.splice(index, 1);
   }
 
+  clearItems() {
+    this.tasks=[];
+    this.nextId = START_ID;
+  }
 }
